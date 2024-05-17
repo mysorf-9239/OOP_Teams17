e1fd655036462e9516e6843d877ae7d9562bd928
Mysorf <uydanh214@gmail.com>
2024-05-18 00:39:45 +0700

    Add: Spidernet image and class.

---
+++ src/model/Object/Obj_Spidernet.java
@@ -0,0 +1,15 @@
+package model.Object;
+
+import model.entity.Entity;
+import view.GamePanel;
+
+public class Obj_Spidernet extends Entity {
+
+    public Obj_Spidernet(GamePanel gp) {
+        super(gp);
+
+        name = "Spidernet";
+        down[1] = setup("/objects/Spidernet");
+    }
+
+}
